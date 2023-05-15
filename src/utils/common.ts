2dff2cbd4c512bb429ae6fb6701fb417007d4428
Eugene Maslovich <ehpc@em42.ru>
2023-05-15 16:20:18 +0300

    fix(common): Pooosuffix

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -3,23 +3,22 @@ export const getPlaceSuffix = (place: number | null | undefined, lang: string): 
       return '';
   }
   const langLower = lang.toLowerCase();
-  let suffix = '';
 
   if (langLower.startsWith('en')) {
       const j = place % 10;
       const k = place % 100;
       if (j === 1 && k !== 11) {
-          suffix = 'st';
+          return 'st';
       } else if (j === 2 && k !== 12) {
-          suffix = 'nd';
+          return 'nd';
       } else if (j === 3 && k !== 13) {
-          suffix = 'rd';
+          return 'rd';
       } else {
-          suffix = 'th';
+          return 'th';
       }
   } else if (langLower.startsWith('ru')) {
-      suffix = '-е';
+      return '-е';
   }
 
-  return suffix;
+  return '';
 };
